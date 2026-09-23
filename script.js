d8964c598b0cc60a4643de63579becaccb648882
Mithrajit Kathir <mithrajit07@gmail.com>
2026-09-23 17:24:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+for (let i = 0; i < codes.length; i++) {
+
+    codes[i].addEventListener("input", function () {
+
+        if (codes[i].value !== "" && i < codes.length - 1) {
+            codes[i + 1].focus();
+        }
+
+    });
+
+    codes[i].addEventListener("keydown", function (event) {
+
+        if (event.key === "Backspace" && codes[i].value === "" && i > 0) {
+            codes[i - 1].value = "";
+            codes[i - 1].focus();
+        }
+
+    });
+
+}
